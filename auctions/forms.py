b49ae86da403806a6df090c9b5bb7b24d15b89f3
ალექსანდრე მჭედლიძე <mchedlidze.aleksandr@students.gov.ge>
2021-07-30 14:09:47 +0400

    now on forms labels are hidden, customized model forms

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,6 +1,7 @@
 from django.db.models.base import Model
 from .models import Listing, Bid, Comment
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
+from django import forms
 
 class NewListingForm(ModelForm):
     class Meta:
@@ -8,12 +9,22 @@ class NewListingForm(ModelForm):
         fields = ['title', 'description', 'starting_bid', 'image_url', 'category']
 
 class NewBidForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+            super(NewBidForm, self).__init__(*args, **kwargs)
+            self.fields['offer'].widget.attrs = {'class': 'form-control', 'placeholder': 'Enter an offer...' }
+            self.fields['offer'].label = ''
+    
     class Meta:
         model = Bid
         fields = ['offer']
 
     
 class NewCommentForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(NewCommentForm, self).__init__(*args, **kwargs)
+        self.fields['comment'].widget.attrs = {'class':'form-control', 'placeholder': 'Write a comment...',}
+        self.fields['comment'].label = ''
+
     class Meta:
         model = Comment
         fields = ['comment']
